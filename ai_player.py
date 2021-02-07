502bce887bae7203c3d271f0d06af80d8f4c9d9b
Maria Vasquez <maria.ines.vf@gmail.com>
2021-02-07 12:01:05 -0600

    avances

--- ai_player.py
+++ ai_player.py
@@ -34,7 +34,6 @@ class Ai_player():
                     element = Bunny(0, 0, row, col)
 
                 board[row][col] = element
-        #print(board)
         # Save member variables
         self.c_player = 2
         self.board = board
@@ -48,7 +47,6 @@ class Ai_player():
 
         self.r_goals = [t for row in board
                         for t in row if t.tile == 2]
-        print(self.r_goals)
         self.g_goals = [t for row in board
                         for t in row if t.tile == 1]
 
@@ -59,83 +57,6 @@ class Ai_player():
         """print("Turn time limit:", self.t_limit)
         print("Max ply depth:", self.ply_depth)"""
         print()
-
-
-    def minimax(self, depth, player_to_max, max_time, a=float("-inf"),
-                b=float("inf"), maxing=True, prunes=0, boards=0):
-
-        # Bottomed out base case
-        if depth == 0 or self.win_analyzer() or time.time() > max_time:
-            return self.utility_distance(player_to_max), None, prunes, boards
-
-        # Setup initial variables and find moves
-        best_move = None
-        if maxing:
-            best_val = float("-inf")
-            moves = self.get_next_moves(player_to_max)
-        else:
-            best_val = float("inf")
-            moves = self.get_next_moves((2
-                    if player_to_max == 1 else 1))
-        # For each move
-        for move in moves:
-            for to in move["to"]:
-                # Bail out when we're out of time
-                if time.time() > max_time:
-                    return best_val, best_move, prunes, boards
-
-                # Move piece to the move outlined
-                piece = move["from"].piece
-                move["from"].piece = 0
-                to.piece = piece
-                boards += 1
-
-                # Recursively call self
-                #se vuelve a llamar de acuerdo a la profundidad programada para poder ver la mejor jugada a largo plazo
-                val, _, new_prunes, new_boards = self.minimax(depth - 1,
-                    player_to_max, max_time, a, b, not maxing, prunes, boards)
-                prunes = new_prunes
-                boards = new_boards
-
-                # Move the piece back
-                to.piece = 0
-                move["from"].piece = piece
-
-                if maxing and val > best_val:
-                    best_val = val
-                    best_move = (move["from"].coord, to.coord)
-                    a = max(a, val)
-
-                if not maxing and val < best_val:
-                    best_val = val
-                    best_move = (move["from"].coord, to.coord)
-                    b = min(b, val)
-
-                if b <= a:
-                    return best_val, best_move, prunes + 1, boards
-
-        return best_val, best_move, prunes, boards
-
-    
-
-    def get_next_moves(self, player=1):
-        moves = []  # All possible moves
-        for col in range(self.b_size):
-            for row in range(self.b_size):
-
-                curr_tile = self.board[row][col]
-
-                # Skip board elements that are not the current player
-                if curr_tile.piece != player:
-                    continue
-
-                move = {
-                    "from": curr_tile,
-                    "to": self.get_valid_moves(curr_tile)
-                }
-                moves.append(move)
-
-        return moves
 
     def get_valid_moves(self, tile, calculated=None, adj=True, next_moves=False):
 
@@ -151,7 +72,6 @@ class Ai_player():
 
         if calculated is None:
             calculated = []
-
 
         # Find and save immediately adjacent moves
         for delta_c in range(-1, 2):
@@ -205,61 +125,131 @@ class Ai_player():
 
 
     def win_analyzer(self):
+        win_human=[]
+        win_alba=[]
 
-        if all(g.piece == 1 for g in self.r_goals):
-            return 1
-        elif all(g.piece == 2 for g in self.g_goals):
-            return 2
-        else:
-            return None
-
-
-    def utility_distance(self, player):
-
-        def point_distance(p0, p1):
-            #IDEA: CALCULAR DISTANCIA MÁS CORTA AL ÁREA DE META
-            
-            """print("coord: ",p1,p0)
-            if (player ==2):
-                p1 = (9,9)
-            else:
-                p1 = (0,0)
-            print(math.sqrt((p1[0] - p0[0])**2 + (p1[1] - p0[1])**2))
-            print("----------------")"""
-            return math.sqrt((p1[0] - p0[0])**2 + (p1[1] - p0[1])**2)
-
-        value = 0
+        for g in self.r_goals:
+            ind = (g.piece == 1)
+            win_human.append(ind)
         
-        for col in range(self.b_size):
-            for row in range(self.b_size):
+        if (all(win_human)==True):
+            return 1
+        
+        for g in self.g_goals:
+            ind = (g.piece == 2)
+            win_alba.append(ind)
+        
+        if (all(win_alba)==True):
+            return 2
+        
+        return None
 
-                tile = self.board[row][col]
-
-                if tile.piece == 1:
-                    distances = []
-                    for g in self.r_goals:
-                        if g.piece != 1:
-                            distances.append(point_distance(tile.coord, g.coord))
-                    """distances1 = [point_distance(tile.coord, g.coord) for g in
-                                 self.r_goals if g.piece != 1]"""
-                    value -= max(distances) if len(distances) else -50
-
-                elif tile.piece == 2:
-                    distances = []
-                    for g in self.g_goals:
-                        if g.piece != 2:
-                            distances.append(point_distance(tile.coord, g.coord))
-
-                    """distances1 = [point_distance(tile.coord, g.coord) for g in
-                                 self.g_goals if g.piece != 2]"""
-                    value += max(distances) if len(distances) else -50
-
-        if player == 2:
-            value *= -1
-        #print(value)
-        return value
-    
+  
     def AIba_turn(self):
+
+        def minimax(depth, player_to_max, max_time, a=float("-inf"),
+                b=float("inf"), maxing=True, prunes=0, boards=0):
+
+            def possible_moves(player=1):
+                moves = []  # All possible moves
+                for col in range(self.b_size):
+                    for row in range(self.b_size):
+
+                        curr_tile = self.board[row][col]
+
+                        # Skip board elements that are not the current player
+                        #print(curr_tile.piece)
+                        if curr_tile.piece != player:
+                            continue
+
+                        move = {}
+
+                        move["from"]= curr_tile
+                        move["to"] = self.get_valid_moves(curr_tile)
+                        moves.append(move)
+
+                return moves
+
+            def heuristic_function(player):
+
+                def distance_to_goal(p0):
+                    #IDEA: CALCULAR DISTANCIA MÁS CORTA AL ÁREA DE META
+                    if (player ==2):
+                        p1 = (9,9)
+                        return (math.sqrt((p1[0] - p0[0])**2 + (p1[1] - p0[1])**2))*-1
+                    else:
+                        p1 = (0,0)
+                        return (math.sqrt((p1[0] - p0[0])**2 + (p1[1] - p0[1])**2))
+
+                value = 0
+                
+                for col in range(self.b_size):
+                    for row in range(self.b_size):
+
+                        tile = self.board[row][col]
+
+                        if tile.piece == 1:
+                            distances = distance_to_goal(tile.coord)
+                            value -= distances
+
+                        elif tile.piece == 2:
+                            distances = distance_to_goal(tile.coord)
+                            value += distances
+                return value
+
+            # Bottomed out base case
+            if depth == 0 or self.win_analyzer() or time.time() > max_time:
+                return heuristic_function(player_to_max), None, prunes, boards
+
+            # Setup initial variables and find moves
+            best_move = None
+            if maxing:
+                best_val = float("-inf")
+                moves = possible_moves(player_to_max)
+            else:
+                best_val = float("inf")
+                moves = possible_moves((2
+                        if player_to_max == 1 else 1))
+            # For each move
+            for move in moves:
+                for to in move["to"]:
+                    # Bail out when we're out of time
+                    if time.time() > max_time:
+                        return best_val, best_move, prunes, boards
+
+                    # Move piece to the move outlined
+                    piece = move["from"].piece
+                    move["from"].piece = 0
+                    to.piece = piece
+                    boards += 1
+
+                    # Recursively call self
+                    #se vuelve a llamar de acuerdo a la profundidad programada para poder ver la mejor jugada a largo plazo
+                    val, _, new_prunes, new_boards = minimax(depth - 1,
+                        player_to_max, max_time, a, b, not maxing, prunes, boards)
+                    prunes = new_prunes
+                    boards = new_boards
+
+                    # Move the piece back
+                    to.piece = 0
+                    move["from"].piece = piece
+
+                    if maxing and val > best_val:
+                        best_val = val
+                        best_move = (move["from"].coord, to.coord)
+                        a = max(a, val)
+
+                    if not maxing and val < best_val:
+                        best_val = val
+                        best_move = (move["from"].coord, to.coord)
+                        b = min(b, val)
+
+                    if b <= a:
+                        return best_val, best_move, prunes + 1, boards
+
+            return best_val, best_move, prunes, boards
+
+        
         # Print out search information
         current_turn = (self.total_plies // 2) + 1
         print("Turn de Al-ba")
@@ -272,7 +262,7 @@ class Ai_player():
 
         # Execute minimax search
         start = time.time()
-        _, move, prunes, boards = self.minimax(self.ply_depth,
+        _, move, prunes, boards = minimax(self.ply_depth,
             self.c_player, max_time)
         end = time.time()
 
