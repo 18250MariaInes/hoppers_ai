be8127a9c9f65ecebac1c6cc414f088a3258da01
Maria Vasquez <maria.ines.vf@gmail.com>
2021-02-07 22:25:08 -0600

    casi

--- ai_player.py
+++ ai_player.py
@@ -16,129 +16,111 @@ from board_game_components import Board, Bunny
 class Ai_player():
 
     def __init__(self):
-        self.b_size = 10
-        self.t_limit = 20
-        
-        # Create initial board
-        board = [[None] * self.b_size for _ in range(self.b_size)]
-        
-        for row in range(self.b_size):
-            for col in range(self.b_size):
-
-                if row + col < 5:
-                    element = Bunny(2, 2, row, col)
-                elif 1 + row + col > 2 * (self.b_size - 3):
-                    
-                    element = Bunny(1, 1, row, col)
-                else:
-                    element = Bunny(0, 0, row, col)
-
-                board[row][col] = element
-        # Save member variables
-        self.c_player = 2
-        self.board = board
-        self.current_player = 1
+        #inicialización de variables
+        self.size_of_board = 10
+        self.time_max_to_compute = 20
         self.valid_moves = []
         self.computing = False
-        self.total_plies = 0
-
+        self.depth_of_game = 0
+        self.aiba_player = 2
         self.ply_depth = 3
-        #self.ab_enabled = True
-
-        self.r_goals = [t for row in board
-                        for t in row if t.tile == 2]
-        self.g_goals = [t for row in board
-                        for t in row if t.tile == 1]
-
+        self.board = Board.initialize_game_board(self.size_of_board)
+        self.current_player = 1
+        self.aiba_zone, self.human_zone =  Board.set_gamer_territory(self.board)
 
         # Print initial program info
         print("Hoppers game with AI by María Inés Vásquez")
         print("Playing against: AI-ba")
-        """print("Turn time limit:", self.t_limit)
-        print("Max ply depth:", self.ply_depth)"""
         print()
 
+    #función para calcular movimiento válidos
     def get_valid_moves(self, tile, calculated=None, adj=True, next_moves=False):
 
+        #calcular pasos
         def step_moves(coord, delta_r, delta_c):
             return(coord.coord[0] + delta_r, coord.coord[1] + delta_c)  
-
+        #calcular saltos
         def jump_moves(row, col, delta_r, delta_c):
             return(row+delta_r, col+delta_c)
-
+        #regresar nueva posición
         def new_position(r, c):
             return self.board[r][c]
 
-
+        #para adjuntar todos los movimientos se inicializa un arreglo
         if calculated is None:
             calculated = []
 
-        # Find and save immediately adjacent moves
+        # calcular todos los movimientos y solo regresar los validos
         for delta_c in range(-1, 2):
             for delta_r in range(-1, 2):
 
-                # Check adjacent tiles
-
+                #pasos
                 step_r, step_c = step_moves(tile, delta_r, delta_c)
-                #jumps
+                #saltos
                 jump_r, jump_c = jump_moves(step_r, step_c, delta_r, delta_c)
 
-                # Skip checking degenerate values
-                #para revisar que no me estoy saliendo del tablero
-                if ((step_r == tile.coord[0] and step_c == tile.coord[1]) or step_r < 0 or step_c < 0 or step_r >= self.b_size or step_c >= self.b_size or jump_r < 0 or jump_c < 0 or jump_r >= self.b_size or jump_c >= self.b_size):
+                #para revisar que me mantengo en el área del tablero o si el movimiento no es válido
+                if ((step_r == tile.coord[0] and step_c == tile.coord[1]) or step_r < 0 or step_c < 0 or step_r >= self.size_of_board or step_c >= self.size_of_board or jump_r < 0 or jump_c < 0 or jump_r >= self.size_of_board or jump_c >= self.size_of_board):
                     continue
 
-                # Handle moves out of/in to goals
+                # Mover pieza
                 new_tile = new_position(step_r,step_c)
                 
                 if new_tile.piece == 0:
-                    if adj:  # Don't consider adjacent on subsequent calls 
-                    #si hay un movimiento para seguirle dando
+                    if adj:  # si ya se inicializo la busqueda, calcular saltos o pasos seguidos
                         calculated.append(new_tile)
                     continue
 
-                # Handle returning moves and moves out of/in to goals
+                # Mover pieza
                 new_tile = new_position(jump_r,jump_c)
+                #si ya fue calculado se excluye
                 if new_tile in calculated: 
                     continue
 
+                #si el espacio esta disponible, se agrega al arreglo y se sigue calculando hasta terminar todos los posibles
                 if new_tile.piece == 0:
                     calculated.append(new_tile)
                     self.get_valid_moves(new_tile, calculated, False)
 
         return calculated
 
+#función para mover conejo
     def bunny_step(self, from_coord, to_coord):
 
+    #cambio de paso o salto del conejo
         def new_location_bunny(from_coord, to_coord):
             # Move piece
             to_coord.piece = from_coord.piece
             from_coord.piece = 0
-            self.total_plies += 1
+            self.depth_of_game += 1
         
-        # Handle trying to move a non-existant piece and moving into a piece
+        # Si la pieza no existe o donde desea moverlo está ocupado regresa mov no válido
         if from_coord.piece == 0 or to_coord.piece != 0:
             print("Movimiento inválido, vuelva a ingresar valores")
             return False
-
+        #movimiento realizado
         new_location_bunny(from_coord, to_coord) 
 
 
+#función para calcular ganador
     def win_analyzer(self):
         win_human=[]
         win_alba=[]
-
-        for g in self.r_goals:
+        # por cada pieza del área de AI-ba se verifica si todas están ocupadas por humano
+        for g in self.aiba_zone:
             ind = (g.piece == 1)
             win_human.append(ind)
         
+        #si todas dan verdadero gana humano
         if (all(win_human)==True):
             return 1
         
-        for g in self.g_goals:
+        # por cada pieza del área del humano se verifica si todas están ocupadas por AI-ba
+        for g in self.human_zone:
             ind = (g.piece == 2)
             win_alba.append(ind)
-        
+
+        #si todas dan verdadero gana AI-ba
         if (all(win_alba)==True):
             return 2
         
@@ -151,29 +133,22 @@ class Ai_player():
                 b=float("inf"), maxing=True, prunes=0, boards=0):
 
             def possible_moves(player=1):
-                moves = []  # All possible moves
-                for col in range(self.b_size):
-                    for row in range(self.b_size):
-
+                moves = []  #calculo de posibles movimientos del jugador en turno
+                for col in range(self.size_of_board):
+                    for row in range(self.size_of_board):
                         curr_tile = self.board[row][col]
-
-                        # Skip board elements that are not the current player
-                        #print(curr_tile.piece)
                         if curr_tile.piece != player:
                             continue
-
                         move = {}
-
                         move["from"]= curr_tile
                         move["to"] = self.get_valid_moves(curr_tile)
                         moves.append(move)
-
                 return moves
 
             def heuristic_function(player):
 
                 def distance_to_goal(p0):
-                    #IDEA: CALCULAR DISTANCIA MÁS CORTA AL ÁREA DE META
+                    #funcion heuristica se basa en escoger la distancia que más avance hasta la esquina del jugador opuesto
                     if (player ==2):
                         p1 = (9,9)
                         return (math.sqrt((p1[0] - p0[0])**2 + (p1[1] - p0[1])**2))*-1
@@ -183,8 +158,8 @@ class Ai_player():
 
                 value = 0
                 
-                for col in range(self.b_size):
-                    for row in range(self.b_size):
+                for col in range(self.size_of_board):
+                    for row in range(self.size_of_board):
 
                         tile = self.board[row][col]
 
@@ -197,12 +172,13 @@ class Ai_player():
                             value += distances
                 return value
 
-            # Bottomed out base case
+            # regresar mejor valor en el caso que se ha llegado hasta el fondo, se ha ganado o el tiempo de ejecución a superado al máximo
             if depth == 0 or self.win_analyzer() or time.time() > max_time:
                 return heuristic_function(player_to_max), None, prunes, boards
 
-            # Setup initial variables and find moves
             best_move = None
+
+            #calculo de los posibles movimientos del max y el min
             if maxing:
                 best_val = float("-inf")
                 moves = possible_moves(player_to_max)
@@ -210,86 +186,87 @@ class Ai_player():
                 best_val = float("inf")
                 moves = possible_moves((2
                         if player_to_max == 1 else 1))
-            # For each move
+            # por cada movida se calcula si el tiempo ha sido superado, si no se sigue evaluando
             for move in moves:
                 for to in move["to"]:
-                    # Bail out when we're out of time
                     if time.time() > max_time:
                         return best_val, best_move, prunes, boards
 
-                    # Move piece to the move outlined
+                    # Movimiento de pieza en el tablero
                     piece = move["from"].piece
                     move["from"].piece = 0
                     to.piece = piece
                     boards += 1
 
-                    # Recursively call self
-                    #se vuelve a llamar de acuerdo a la profundidad programada para poder ver la mejor jugada a largo plazo
+                    #se vuelve a llamar recurivamente, per turnarndo al not maxing para desarrollar todos los nodos hasta la profundidad programada
                     val, _, new_prunes, new_boards = minimax(depth - 1,
                         player_to_max, max_time, a, b, not maxing, prunes, boards)
                     prunes = new_prunes
                     boards = new_boards
 
-                    # Move the piece back
+                    # Se regresa la posición dado que no se ha seleccionado aún
                     to.piece = 0
                     move["from"].piece = piece
 
+                    #si estamos en maxing y se consiguio un mejor movimiento se guarda hasta terminar de recorrer todo
                     if maxing and val > best_val:
                         best_val = val
                         best_move = (move["from"].coord, to.coord)
                         a = max(a, val)
 
+                    #si estamos en min y se consiguio un mejor movimiento se guarda hasta terminar de recorrer todo
                     if not maxing and val < best_val:
                         best_val = val
                         best_move = (move["from"].coord, to.coord)
                         b = min(b, val)
-
+                    #funcion de alpha-beta prunning para recortar el árbol de decisión
                     if b <= a:
                         return best_val, best_move, prunes + 1, boards
-
+            #luego de terminar de revisar todos los movimientos se regresa el mejor movimiento encontrado para el jugador
             return best_val, best_move, prunes, boards
 
         
-        # Print out search information
-        current_turn = (self.total_plies // 2) + 1
-        print("Turn de Al-ba")
+        # Inicio de turno de AI-ba
+        #inicialización de turno
+        current_turn = (self.depth_of_game // 2) + 1
+        print("Turn de AI-ba")
         print("--------------------------")
         print("Calculando ...")
-
-        # self.board_view.set_status("Computing next move...")
         self.computing = True
-        max_time = time.time() + self.t_limit
+        max_time = time.time() + self.time_max_to_compute
 
-        # Execute minimax search
+        # Se realiza el minimax de sus movimientos
         start = time.time()
         _, move, prunes, boards = minimax(self.ply_depth,
-            self.c_player, max_time)
+            self.aiba_player, max_time)
         end = time.time()
 
-        """MOVE ES EL MOVIMIENTO DE AI"""
+        #movimiento seleccionado se ejecuta
         print("Al-ba se ha movido de "+str(move[0])+" a "+str(move[1]))
         move_from = self.board[move[0][0]][move[0][1]]
         move_to = self.board[move[1][0]][move[1][1]]
         self.bunny_step(move_from, move_to)
-
+        #se calcula ganador, si ganó AI-ba se termina el juego
         winner = self.win_analyzer()
         if winner:
             print("AI-BA HA GANADO!")
             self.current_player = None
-
-        else:  # Toggle the current player
+        #si no ha ganado se cede el turno al jugador humano
+        else: 
             self.current_player = (2
                 if self.current_player == 1 else 1)
 
         self.computing = False
         print()
     
-    #mi jugada
+    #Jugada de humano
     def human_player_move(self):
-        current_turn = (self.total_plies // 2) + 1
+        current_turn = (self.depth_of_game // 2) + 1
         validation = True
         print("¡Tu turno!")
         print("----------------------------------")
+
+        #ingreso de datos para mover conejo/ficha
 
         move_from_row = int(input("Ingrese fila de la pieza a mover:"))
         move_from_col = int(input("Ingrese columna de la pieza a mover :"))
@@ -297,26 +274,33 @@ class Ai_player():
         move_to_row = int(input("Ingrese fila a donde desea moverla:"))
         move_to_col = int(input("Ingrese columna a donde desea moverla :"))
         
+        #se obtiene la pieza del tablero que desea mover
         move_from = self.board[move_from_row][move_from_col]
+        #se calcula los movimientos validos desde ese
         self.valid_moves = self.get_valid_moves(move_from)
-
+        #se obtiene la pieza del tablero de a donde desea moverse
         move_to = self.board[move_to_row][move_to_col]
         print("Te has movido de ("+str(move_from_row)+","+str(move_from_col)+") a ("+str(move_to_row)+","+str(move_to_col)+")")
+        #si la nueva pieza no esta entre los validos vuelve a repetirse el turno
         if (move_to not in self.valid_moves):
             print("Ese movimiento no es válido")
             return
         else:
+            #si es valido se mueve
             validation = self.bunny_step(move_from, move_to)
 
         winner = self.win_analyzer()
+        #si regresa que humanos es ganador se para el juego
         if winner:
             print("LE HAS GANADO A AIBA!")
             self.current_player = None
+        #si la validacion es false vuelve a repetirse su turno
         elif (validation==False):
             self.human_player_move()
-        elif self.c_player is not None:
+        #si sigue jugando y la movida fue valida le ceda el turno a AI-ba
+        elif self.aiba_player is not None:
             self.AIba_turn()
-        else:  # Toggle the current player
+        else:  #control de jugador actual
             self.current_player = (2
                 if self.current_player == 1 else 1)
         
--- board_game_components.py
+++ board_game_components.py
@@ -4,7 +4,7 @@ María Inés Vásquez Figueroa
 AI-ba player for Hopper board game
 Board game components
 """
-
+#clase del tablero de juego
 class Board():
 
     def __init__(self, init_board, *args, **kwargs):
@@ -12,8 +12,35 @@ class Board():
         self.tiles = {}
         self.board = init_board
         self.b_size = len(init_board)
-        
 
+
+    #inicializar el tablero
+    def initialize_game_board(size_of_board):
+        board = [[None] * size_of_board for _ in range(size_of_board)]
+        
+        for row in range(size_of_board):
+            for col in range(size_of_board):
+
+                if row + col < 5:
+                    element = Bunny(2, 2, row, col)
+                elif 1 + row + col > 2 * (size_of_board - 3):
+                    
+                    element = Bunny(1, 1, row, col)
+                else:
+                    element = Bunny(0, 0, row, col)
+                board[row][col] = element
+        
+        return board
+    #calcular área de victoria
+    def set_gamer_territory(board):
+        aiba_zone = [a for row in board
+                        for a in row if a.tile == 2]
+        human_zone = [h for row in board
+                        for h in row if h.tile == 1]
+        return aiba_zone, human_zone
+
+        
+#clase de las piezas que son conejos
 class Bunny():
 
     def __init__(self, tile=0, piece=0, row=0, col=0):
